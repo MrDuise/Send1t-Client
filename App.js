7285f4633bd1cb298f792d2756a1c08397ff0d73
Michael Duisenberg <mrduise@gmail.com>
2023-01-08 21:17:21 -0700

    made a conversation card
    made a conversation card and got it to display default text

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import ConversationCard from './src/features/conversation-log/components/conversation-card'
+import React from 'react'
+import {theme} from './src/infrastructure/theme'
+import  ConversationsLog  from './src/features/conversation-log/screens/conversation-log.screen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+    <SafeAreaView style={styles.container}>
+      
+      <ConversationsLog />
       <StatusBar style="auto" />
-    </View>
+   
+    </SafeAreaView>
+    </>
   );
 }
 
@@ -14,7 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
 });
--- src/features/conversation-log/components/conversation-card.js
+++ src/features/conversation-log/components/conversation-card.js
@@ -1,39 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import styled from 'styled-components/native';
-import {  Card, Text } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import React from 'react';
+
+import { theme } from '../../../infrastructure/theme';
+import { Card, Text, Avatar } from 'react-native-paper';
 /**
  * Takes a conversation and displays it in a card format with the contacts name and the most recent message sent
  * Also displays the time the conversation was updated
  * Allows a user to click on the message to view the conversation in a new screen where they can reply to the message
  *
- * @return {*} 
+ * @return {*}
  */
-const ConversationCard = ({conversation = {}}) => {
-const { participants = [{userName : "JohnSmith"}, {userName: "MikeDo"}], messages, isGroup = false, dateUpdated, admin = "JohnSmith" } = conversation
 
-// If the conversation is a group conversation, the title will be the names of all the participants
-// If the conversation is a one on one conversation, the title will be the name of the other participant
+const ConversationCard = ({ conversation = {} }) => {
+  const {
+    participants = [{ userName: 'JohnSmith' }, { userName: 'MikeDo' }],
+    messages = ['Hello There', 'Hey hows it going man?'],
+    isGroup = false,
+    dateUpdated = '01/05/23',
+    admin = 'JohnSmith',
+  } = conversation;
 
-//TODO: the one on one conversation title will need work for the second participant
-const title = isGroup ? participants.map(participant => participant.userName).join(', ') : participants[1].userName
+  // If the conversation is a group conversation, the title will be the names of all the participants
+  // If the conversation is a one on one conversation, the title will be the name of the other participant
+
+  //TODO: the one on one conversation title will need work for the second participant
+  const title = isGroup
+    ? participants.map((participant) => participant.userName).join(', ')
+    : participants[1].userName;
 
   return (
-    <Card>
-      <Card.Title title="Card Title" subtitle="Card Subtitle" />
-      <Card.Content>
-        <Title>{title}</Title>
-        <Paragraph>Card content</Paragraph>
-      </Card.Content>
-      <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-      <Card.Actions>
-        <Button>Cancel</Button>
-        <Button>Ok</Button>
-      </Card.Actions>
+    <Card.Title
+      style={styles.card}
+      title={title}
+      subtitle={dateUpdated}
+      left={(props) => <Avatar.Text {...props} size={32} label={title[0]} />}
+      right={(props) => <Text {...props}>{messages[1]}</Text>}
+    />
+  );
+};
 
+export default ConversationCard;
 
-      </Card>
-  )
-}
-
-export default ConversationCard
+const styles = StyleSheet.create({
+  card: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: theme.colors.ui.primary,
+    borderRadius: 10,
+    flexDirection: 'row',
+    backgroundColor: theme.colors.bg.primary,
+    padding: theme.sizes[1],
+    fontSize: theme.fontSizes[1],
+  },
+});
