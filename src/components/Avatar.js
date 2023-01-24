c8f180ed146ed8ddada6c62b683241228095590a
Michael Duisenberg <mrduise@gmail.com>
2023-01-23 17:09:13 -0700

    updated Avatar file

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,20 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Avatar, Badge } from 'react-native-paper';
 import React, {useState} from 'react';
-
-const AvatarIcon = ({ profilePic= "", onlineStatus = false }) => {
+/**
+ * This component is used to display a users Avatar Icon with a online status
+ * If the user does not have a photo, then it display a simple account Icon
+ * @param {*} { profilePic = "", onlineStatus = true }
+ * @return {*} 
+ */
+const AvatarIcon = ({ profilePic = "", onlineStatus = true, size=32 }) => {
 
   return (
     <>
       {profilePic !== "" ? (
         <>
         
-        <Badge size={12} style={{ position: 'absolute', top: 0, right: 0, backgroundColor: 'green' }}> </Badge>
+        <Badge size={12} style={styles(onlineStatus).statusState}> </Badge>
         </>
       ) : (
         <>
-        <Avatar.Icon size={32} icon="account" />
-        <Badge size={12} style={styles.statusState}> </Badge>
+        <Avatar.Icon size={size} icon="account" />
+        <Badge size={size - 20} style={styles(onlineStatus).statusState}> </Badge>
         </>
       )}
     </>
@@ -23,11 +28,11 @@ const AvatarIcon = ({ profilePic= "", onlineStatus = false }) => {
 
 export default AvatarIcon;
 
-const styles = StyleSheet.create({
+const styles = (onlineStatus) => StyleSheet.create({
     statusState: {
         position: 'absolute',
         bottom: 0,
         right: 3,
-        backgroundColor: onlineStatus ? 'green' : 'red',
+        backgroundColor: onlineStatus === true ? 'green' : 'red',
     }
 });
---
+++ src/infrastructure/backend/request.js
@@ -0,0 +1,23 @@
+/** This Page handles all the api requests, so they can easily be called else where */
+
+const login = () => {
+
+}
+
+const googleLogin = () => {
+
+}
+/**
+ *Gets the users online status
+ *
+ */
+const getStatus = () => {
+
+}
+
+
+module.exports = {
+    login,
+    googleLogin,
+    getStatus
+}
