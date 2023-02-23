573d910b157b0a782e986818960677e1477dd396
Michael Duisenberg <mrduise@gmail.com>
2023-02-22 23:30:28 -0700

    updated the login screen
    added in navigator component and logic for the implementation of the login screen

--- src/features/login/screens/login.screen.js
+++ src/features/login/screens/login.screen.js
@@ -1,45 +1,73 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
-import React from 'react'
+import React, { useState } from 'react';
 
 import { theme } from '../../../infrastructure/theme';
 
-import Logo from '../../../components/Logo'
+import { useNavigation } from '@react-navigation/native';
 
-const Login = () => {
+import { login } from '../../../infrastructure/backend/request';
+
+import Logo from '../../../components/Logo';
+
+const Login = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  //takes the username and password and sends it to the backend
+  //if the login is successful, it will navigate to the ConversationsLog screen
+  const handleLogin = async () => {
+    setLoading(true);
+    try {
+      const response = await login(username, password);
+      const data = await response.json();
+
+      if (data !== null) {
+        navigation.navigate('ConversationLog');
+      } else {
+        alert('Invalid username or password');
+      }
+    } catch (error) {
+      alert('An error occurred');
+      console.error(error);
+    }
+    setLoading(false);
+   
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-        <Logo />
-        <Text style={styles.title}>Login</Text>
-        <Text style={styles.muted}>Please Sign in to conntinue</Text>
-        <TextInput
-          label="Username"
-          mode="outlined"
-          style={styles.textInput}
-          left={<TextInput.Icon icon="account" />}
-        />
-        <TextInput
-          label="Password"
-          mode="outlined"
-          secureTextEntry={true}
-          style={styles.textInput}
-          left={<TextInput.Icon icon="lock" />}
-        />
-      
-        <Button mode="contained" style={styles.buttonLogin}>
-          Login
-        </Button>
-        <Button mode="contained" style={styles.registerButton}>
-          Register
-        </Button>
+      <Logo />
+      <Text style={styles.title}>Login</Text>
+      <Text style={styles.muted}>Please Sign in to conntinue</Text>
+      <TextInput
+        label="Username"
+        mode="outlined"
+        value={username}
+        style={styles.textInput}
+        left={<TextInput.Icon icon="account" />}
+      />
+      <TextInput
+        label="Password"
+        mode="outlined"
+        value={password}
+        secureTextEntry={true}
+        style={styles.textInput}
+        left={<TextInput.Icon icon="lock" />}
+      />
 
-
-        
+      <Button mode="contained" onPress={handleLogin} loading={loading}>
+        Login
+      </Button>
+      <Button mode="contained" style={styles.registerButton}>
+        Register
+      </Button>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
 const styles = StyleSheet.create({
   container: {
@@ -47,7 +75,6 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.colorTheme.purple,
     alignItems: 'center',
     justifyContent: 'center',
-    
   },
   title: {
     fontSize: 30,
@@ -71,6 +98,5 @@ const styles = StyleSheet.create({
   },
   registerButton: {
     width: 200,
-  }
-
-})
+  },
+});
--- src/infrastructure/backend/request.js
+++ src/infrastructure/backend/request.js
@@ -1,23 +1,40 @@
 /** This Page handles all the api requests, so they can easily be called else where */
 
-const login = () => {
+const login = async (userName, password) => {
+  try {
+    const response = await fetch('http://localhost:8000/v1/users/login/local', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userName: userName,
+        password: password,
+      }),
+    });
+    const data = await response.json();
+    if (data) {
+      const { token } = data.token;
+      return token;
+    } else {
+      console.log(data);
+      return null;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-}
-
-const googleLogin = () => {
-
-}
+const googleLogin = () => {};
 /**
  *Gets the users online status
  *
  */
-const getStatus = () => {
-
-}
-
+const getStatus = () => {};
 
 module.exports = {
-    login,
-    googleLogin,
-    getStatus
-}
+  login,
+  googleLogin,
+  getStatus,
+};
