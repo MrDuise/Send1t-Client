224d680878fe028a86911bdfc9b4f41131e2a2ee
Michael Duisenberg <mrduise@gmail.com>
2023-03-18 20:23:49 -0700

    updated the conversation cards
    implemented a test press event to each card, allowing it to be clicked. also did the same thing with the avatar selection

--- src/features/conversation-log/components/conversation-card.js
+++ src/features/conversation-log/components/conversation-card.js
@@ -1,9 +1,10 @@
-import { View, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, StyleSheet,Pressable } from 'react-native';
+import React, {useState, useEffect} from 'react';
 
 import { theme } from '../../../infrastructure/theme';
 import { Card, Text, Avatar } from 'react-native-paper';
 import AvatarIcon from '../../../components/Avatar'
+import { getMessages } from '../../../infrastructure/backend/request';
 /**
  * Takes a conversation and displays it in a card format with the contacts name and the most recent message sent
  * Also displays the time the conversation was updated
@@ -14,7 +15,7 @@ import AvatarIcon from '../../../components/Avatar'
 
 //TODO: add a prop to the conversation card that will allow the user to click on the card and view the conversation
 //TODO: set the message to be the last message sent in the conversation
-const ConversationCard = ({ conversation = {} }) => {
+const ConversationCard = ({ conversation = {}, seen = false }) => {
   const {
     participants = [{ userName: 'JohnSmith' }, { userName: 'MikeDo' }],
     messages = ['Hello There', 'Hey hows it going man?'],
@@ -22,6 +23,24 @@ const ConversationCard = ({ conversation = {} }) => {
     dateUpdated = '01/05/23',
     admin = 'JohnSmith',
   } = conversation;
+
+  useEffect(() => {
+    const getMessagesFromAPI = async () => {
+      const messages = await getMessages(conversation._id);
+      setMessage(messages);
+    };
+    getMessagesFromAPI();
+  }, []);
+
+  
+
+  const [message, setMessage] = useState(messages[1]);
+  const [read, setRead] = useState(seen);
+
+  const readConvo = (conversation) => {
+    setRead(true);
+    console.log(conversation._id);
+  };
 
   // If the conversation is a group conversation, the title will be the names of all the participants
   // If the conversation is a one on one conversation, the title will be the name of the other participant
@@ -31,26 +50,24 @@ const ConversationCard = ({ conversation = {} }) => {
     ? participants.map((participant) => participant.userName).join(', ')
     : participants[0];
   return (
+    <Pressable onPress={() => readConvo(conversation)}>
     <Card.Title
-      style={styles.card}
+      style={styles(read).card}
       title={title}
       subtitle={dateUpdated}
       left={(props) => <AvatarIcon />}
       right={(props) => <Text {...props}>{messages[1]}</Text>}
     />
+    </Pressable>
   );
 };
 
 export default ConversationCard;
 
-const styles = StyleSheet.create({
+const styles = (read) => StyleSheet.create({
   card: {
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderColor: theme.colors.ui.primary,
-    borderRadius: 10,
     flexDirection: 'row',
-    backgroundColor: theme.colors.bg.primary,
+    backgroundColor: read === true ? theme.colors.bg.primary : theme.colors.bg.secondary,
     padding: theme.sizes[1],
     fontSize: theme.fontSizes[1],
   },
